8565d0e934506d441f037bbcfd2436f2410dae4c
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-09-06 10:29:38 +0700

    check

---
+++ testcases/com/nopcommerce/user/Level_03_Page_Object_Login.java
@@ -0,0 +1,56 @@
+package com.nopcommerce.user;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import common.BasePage;
+import pageObjects.HomePageObject;
+import pageObjects.RegisterPageObject;
+
+public class Level_03_Page_Object_Login extends BasePage {
+	private WebDriver driver;
+	private String firstName, lastName, password, emailAddress;
+
+	private HomePageObject homePage;
+	private RegisterPageObject registerPage;
+	private String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		homePage = new HomePageObject(driver);
+		registerPage = new RegisterPageObject(driver);
+
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://demo.nopcommerce.com/");
+		firstName = "Automation";
+		lastName = "FC";
+		emailAddress = "afc" + generateRandomNumber() + "@gmail.com";
+		password = "123456";
+
+	}
+
+	@Test
+	public void Login_01_Empty_Data() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int generateRandomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(9999);
+	}
+
+}
