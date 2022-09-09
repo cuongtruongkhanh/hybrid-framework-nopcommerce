a14814a09e8de984b0a5eaf6cddcb4f026f5820b
cuong.truong <khanhcuong.hanu@gmail.com>
2022-09-09 12:51:23 +0700

    Page Object Login - log activities

--- testcases/com/nopcommerce/user/Level_03_Page_Object_Login.java
+++ testcases/com/nopcommerce/user/Level_03_Page_Object_Login.java
@@ -68,82 +68,91 @@ public class Level_03_Page_Object_Login extends BasePage {
 
 	@Test
 	public void Login_01_Empty_Data() {
+		System.out.println("Login_01_Empty_Data - Step 01: Click to Login link");
 		homePage.clickToLoginLink();
 		// từ trang Home - Click Login link -> qua trang Login
 		loginPage = new LoginPageObject(driver);
 
+		System.out.println("Login_01_Empty_Data - Step 02: Click to Login button");
 		loginPage.clickToLoginButton();
-
+		System.out.println("Login_01_Empty_Data - Step 03: Verify error message display");
 		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextBox(), "Please enter your email");
 
 	}
 
 	@Test
 	public void Login_02_Invalid_Email() {
+		System.out.println("Login_02_Invalid_Email - Step 01: Click to Login link");
 		homePage.clickToLoginLink();
 		// từ trang Home - Click Login link -> qua trang Login
 		loginPage = new LoginPageObject(driver);
-
+		System.out.println("Login_02_Invalid_Email - Step 02: Input invalid email");
 		loginPage.inputToEmailTextbox(invalidEmail);
-
+		System.out.println("Login_02_Invalid_Email - Step 03: Click to Login button");
 		loginPage.clickToLoginButton();
-
+		System.out.println("Login_02_Invalid_Email - Step 04: Verify error message display");
 		Assert.assertEquals(loginPage.getErrorMessageAtEmailTextBox(), "Wrong email");
 	}
 
 	@Test
 	public void Login_03_Email_Not_Exist() {
+		System.out.println("Login_03_Email_Not_Exist - Step 01: Click to Login link");
 		homePage.clickToLoginLink();
 		// từ trang Home - Click Login link -> qua trang Login
 		loginPage = new LoginPageObject(driver);
-
+		System.out.println("Login_03_Email_Not_Exist - Step 02: Input not existing email");
 		loginPage.inputToEmailTextbox(notExistEmail);
-
+		System.out.println("Login_03_Email_Not_Exist - Step 03: Click to Login button");
 		loginPage.clickToLoginButton();
-
+		System.out.println("Login_03_Email_Not_Exist - Step 04: Verify error message display");
 		Assert.assertEquals(loginPage.getErrorMessageUnsucessful(), "Login was unsuccessful. Please correct the errors and try again.\nNo customer account found");
 	}
 
 	@Test
 	public void Login_04_Existing_Email_Empty_Password() {
+		System.out.println("Login_04_Existing_Email_Empty_Password - Step 01: Click to Login link");
 		homePage.clickToLoginLink();
 		// từ trang Home - Click Login link -> qua trang Login
 		loginPage = new LoginPageObject(driver);
-
+		System.out.println("Login_04_Existing_Email_Empty_Password - Step 02: Input existing email");
 		loginPage.inputToEmailTextbox(existingEmail);
 		loginPage.inputToPasswordTextbox("");
-
+		System.out.println("Login_04_Existing_Email_Empty_Password - Step 03: Click to Login button");
 		loginPage.clickToLoginButton();
+		System.out.println("Login_04_Existing_Email_Empty_Password - Step 04: Verify error message display");
 		Assert.assertEquals(loginPage.getErrorMessageUnsucessful(), "Login was unsuccessful. Please correct the errors and try again.\nThe credentials provided are incorrect");
 	}
 
 	@Test
 	public void Login_05_Existing_Email_Incorrect_Password() {
+		System.out.println("Login_05_Existing_Email_Incorrect_Password - Step 01: Click to Login link");
 		homePage.clickToLoginLink();
 		// từ trang Home - Click Login link -> qua trang Login
 		loginPage = new LoginPageObject(driver);
-
+		System.out.println("Login_05_Existing_Email_Incorrect_Password - Step 02: Input existing email with incorrect password");
 		loginPage.inputToEmailTextbox(existingEmail);
 		loginPage.inputToPasswordTextbox(incorrectPassword);
-
+		System.out.println("Login_05_Existing_Email_Incorrect_Password - Step 03: Click to Login button");
 		loginPage.clickToLoginButton();
+		System.out.println("Login_05_Existing_Email_Incorrect_Password - Step 04: Verify error message display");
 		Assert.assertEquals(loginPage.getErrorMessageUnsucessful(), "Login was unsuccessful. Please correct the errors and try again.\nThe credentials provided are incorrect");
 	}
 
 	@Test
 	public void Login_06_Valid_Email_Valid_Password() {
+		System.out.println("Login_06_Valid_Email_Valid_Password - Step 01: Click to Login link");
 		homePage.clickToLoginLink();
 		// từ trang Home - Click Login link -> qua trang Login
 		loginPage = new LoginPageObject(driver);
-
+		System.out.println("Login_06_Valid_Email_Valid_Password - Step 02: Input existing email with valid password");
 		loginPage.inputToEmailTextbox(existingEmail);
 		loginPage.inputToPasswordTextbox(validPassword);
-
+		System.out.println("Login_06_Valid_Email_Valid_Password - Step 03: Click to Login button");
 		loginPage.clickToLoginButton();
 
 		// login thành công -> Homepage
 		homePage = new HomePageObject(driver);
-
+		System.out.println("Login_06_Valid_Email_Valid_Password - Step 04: Verify My Account Link displayed");
 		Assert.assertTrue(homePage.isMyAccountLinkDisplayed());
 	}
 
