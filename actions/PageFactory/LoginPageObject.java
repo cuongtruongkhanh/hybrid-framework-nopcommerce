3a407030c14831b4c320f862d279ff9195a80e27
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-09-13 17:18:54 +0700

    Page Factory

--- actions/PageFactory/LoginPageObject.java
+++ actions/PageFactory/LoginPageObject.java
@@ -1,14 +1,33 @@
 package PageFactory;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-import common.BasePage;
+import common.BasePageFactory;
 
-public class LoginPageObject extends BasePage {
+public class LoginPageObject extends BasePageFactory {
 	private WebDriver driver;
 
 	public LoginPageObject(WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
+
+	@FindBy(xpath = "//input[@id='Email']")
+	private WebElement emailTextbox;
+
+	@FindBy(xpath = "//input[@id='Password']")
+	private WebElement passwordTextbox;
+
+	@FindBy(xpath = "//button[text()='Log in']")
+	private WebElement loginButton;
+
+	@FindBy(xpath = "//span[@id='Email-error']")
+	private WebElement emailErrorMessage;
+
+	@FindBy(xpath = "//div[@class='message-error validation-summary-errors']")
+	private WebElement unsuccessfullErrorMessage;
 
 }
