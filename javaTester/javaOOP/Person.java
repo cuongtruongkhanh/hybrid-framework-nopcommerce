6c97d63548bcdf411b06a3813043eed0d24baebf
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-09-28 17:18:44 +0700

    class object

---
+++ javaTester/javaOOP/Person.java
@@ -0,0 +1,12 @@
+package javaOOP;
+
+public class Person {
+
+	public static void main(String[] args) {
+		Person person = new Person();
+		// Person class
+		// person = instance / biến / object
+
+	}
+
+}
