d29bcd90ab9f0d9473076f904757ee743bebfc43
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-10-13 17:41:10 +0700

    abstract

--- javaTester/javaOOP/overriding/Person.java
+++ javaTester/javaOOP/overriding/Person.java
@@ -1,9 +1,12 @@
 package javaOOP.overriding;
 
 public abstract class Person {
+
+	// Option
 	public void eat() {
 		System.out.println("Suat com 20k");
 	}
 
+	// Must (Template)
 	public abstract void sleep();
 }
