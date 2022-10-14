3970b986f5eeb2acc51b505a207649589347eb1d
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-10-14 16:08:06 +0700

    java oop

---
+++ javaTester/javaOOP/inheritance/Animal.java
@@ -0,0 +1,17 @@
+package javaOOP.inheritance;
+
+public class Animal {
+
+	public Animal() {
+		System.out.println("Parent's constructor");
+	}
+
+	public Animal(String name) {
+		System.out.println("Parent's constructor - " + name);
+	}
+
+	public Animal(String name, String age) {
+		System.out.println("Parent's constructor - " + name);
+		System.out.println("Parent's constructor - " + age);
+	}
+}
---
+++ javaTester/javaOOP/inheritance/Dog.java
@@ -0,0 +1,9 @@
+package javaOOP.inheritance;
+
+public class Dog extends Animal {
+
+	public Dog() {
+		super("Tom", "5");
+		System.out.println("Child's constructor");
+	}
+}
