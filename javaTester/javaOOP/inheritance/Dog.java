ded452c3acbf0e243c10f191239445ec5a6b5440
Truong Khanh Cuong <khanhcuong.hanu@gmail.com>
2022-10-15 22:41:36 +0700

    encapsulation

--- javaTester/javaOOP/inheritance/Dog.java
+++ javaTester/javaOOP/inheritance/Dog.java
@@ -2,7 +2,21 @@ package javaOOP.inheritance;
 
 public class Dog implements Runnable {
 
+	private int age;
+
 	public Dog() {
 		System.out.println("Child's constructor");
+	}
+
+	public void setAge(int age) {
+		if (age > 0) {
+			this.age = age;
+		} else {
+			this.age = 0;
+		}
+	}
+
+	public int getAge() {
+		return this.age;
 	}
 }
--- javaTester/javaOOP/inheritance/Test.java
+++ javaTester/javaOOP/inheritance/Test.java
@@ -4,5 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		HuskyDog dog = new HuskyDog();
 
+		dog.setAge(-15);
+		System.out.println(dog.getAge());
+
+		dog.setAge(15);
+		System.out.println(dog.getAge());
+
 	}
 }
