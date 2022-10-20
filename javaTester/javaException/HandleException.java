d53ec62f1d015d335193b2bcd1a72c59f97dd352
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-10-20 14:02:54 +0700

    exception

--- javaTester/javaException/HandleException.java
+++ javaTester/javaException/HandleException.java
@@ -1,14 +1,34 @@
 package javaException;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class HandleException {
-	public static void main(String[] args) throws IOException {
-		File file = new File("C:\\automationfc.jpg");
+	WebDriver driver;
 
-		FileReader fr = new FileReader(file);
-		fr.close();
+	@Test
+	public void TC_01() throws IOException {
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+
+		driver.get("http://live.techpanda.org/index.php");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		driver.findElement(By.xpath("//a[text()='automation']")).click();
+	}
+
+	@AfterClass
+	public void closeBrowser() {
+		driver.quit();
 	}
 }
