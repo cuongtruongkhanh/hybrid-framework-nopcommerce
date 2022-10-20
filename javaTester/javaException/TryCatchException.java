1b9d9090eea299b5d15d37bc54bc8c42be88f621
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-10-20 16:14:59 +0700

    trycatch

--- javaTester/javaException/TryCatchException.java
+++ javaTester/javaException/TryCatchException.java
@@ -7,8 +7,33 @@ public class TryCatchException {
 		try {
 			// Đúng: chạy hết đoạn code trong try và không catch
 			number = number / 0;
-		} catch (Exception e) {
-			// TODO: handle exception
+		} catch (ArithmeticException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(number);
+
+		String[] browserName = { "Chrome", "Firefox", "Safari" };
+
+		try {
+			browserName[0] = "Edge Chromnium";
+			browserName[3] = "IE";
+
+		} catch (ArrayIndexOutOfBoundsException e) {
+			e.printStackTrace();
+		}
+
+		for (String browser : browserName) {
+			System.out.println(browser);
+		}
+
+		try {
+			int array[] = new int[10];
+			array[9] = 30 / 0;
+		} catch (ArithmeticException e) {
+			System.out.println("Khong chia dc cho 0");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Index vuot ngoai do dai cua mang");
 		}
 
 	}
