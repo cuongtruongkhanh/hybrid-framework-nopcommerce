0e57e429a2325c7cfbeec702fba99f50d4ff846b
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-11-15 14:17:45 +0700

    update

--- javaTester/javaBasic/Topic_18_Rotate_number.java
+++ javaTester/javaBasic/Topic_18_Rotate_number.java
@@ -6,6 +6,8 @@ public class Topic_18_Rotate_number {
 		int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		int[] b = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+		int size = a.length;
+		System.out.println(size);
 		for (int i = 0; i < 10; i++) {
 			b[10 - i - 1] = a[i];
 		}
