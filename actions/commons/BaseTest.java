f901ad038c3f637b47f2dbb13aff2657620b55c4
cuongtruongkhanh <khanhcuong.hanu@gmail.com>
2022-12-26 16:15:29 +0700

    updat browserlist

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -12,10 +12,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -201,6 +203,68 @@ public class BaseTest {
 		} else {
 			throw new RuntimeException("Browser name invalid");
 		}
+		driver.manage().timeouts().implicitlyWait(GlobalConstants.LONG_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(appUrl);
+		return driver;
+	}
+
+	protected WebDriver getBrowserDriver(String browserName, String appUrl, String ipAddress, String portNumber) {
+		BrowserList browserList = BrowserList.valueOf(browserName.toUpperCase());
+		DesiredCapabilities capability = new DesiredCapabilities();
+
+		if (browserList == BrowserList.FIREFOX) {
+			WebDriverManager.firefoxdriver().setup();
+			capability = DesiredCapabilities.firefox();
+			capability.setBrowserName("firefox");
+			capability.setPlatform(Platform.ANY);
+
+			FirefoxOptions options = new FirefoxOptions();
+			options.merge(capability);
+		} else if (browserList == BrowserList.CHROME) {
+			WebDriverManager.chromedriver().setup();
+			capability = DesiredCapabilities.chrome();
+			capability.setBrowserName("chrome");
+			capability.setPlatform(Platform.ANY);
+
+			ChromeOptions options = new ChromeOptions();
+			options.merge(capability);
+		} else if (browserList == BrowserList.EDGE) {
+			WebDriverManager.edgedriver().setup();
+			capability = DesiredCapabilities.chrome();
+			capability.setBrowserName("edge");
+			capability.setPlatform(Platform.ANY);
+
+			EdgeOptions options = new EdgeOptions();
+			options.merge(capability);
+		} else if (browserList == BrowserList.SAFARI) {
+			capability = DesiredCapabilities.safari();
+			capability.setBrowserName("safari");
+			capability.setJavascriptEnabled(true);
+			capability.setPlatform(Platform.ANY);
+		} else if (browserList == BrowserList.OPERA) {
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
+		} else if (browserList == BrowserList.COCCOC) {
+			// tru di 5-6 version cua Coc Coc thi ra version cua ChromeDriver
+			WebDriverManager.chromedriver().driverVersion("104.0.5112.79").setup();
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files\\CocCoc\\Browser\\Application\\browser.exe");
+			driver = new ChromeDriver(options);
+		} else if (browserList == BrowserList.BRAVE) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe");
+			driver = new ChromeDriver(options);
+		} else {
+			throw new RuntimeException("Browser name invalid");
+		}
+		try {
+			driver = new RemoteWebDriver(new URL(String.format("http://%s:%s/wd/hub", ipAddress, portNumber)), capability);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
 		driver.manage().timeouts().implicitlyWait(GlobalConstants.LONG_TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(appUrl);
