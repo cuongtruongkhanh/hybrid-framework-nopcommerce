66ae3ffe06f52ebcc943f9de0383a814fcafb984
cuongtruongkhanh <khanhcuong.hanu@gmail.com>
2022-12-28 00:28:15 +0700

    updated

---
+++ actions/factoryBrowser/ChromeDriverManager.java
@@ -0,0 +1,18 @@
+package factoryBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeDriverManager implements BrowserFactory {
+
+	@Override
+	public WebDriver getBrowserDriver() {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		return new ChromeDriver();
+	}
+
+}
